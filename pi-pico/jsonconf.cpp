a72b43ff5d509cac757539b5c76bfc9e1eaaa8b6
Martin Schaller <dh1df@darc.de>
2023-06-06 15:33:52 +0200

    Make client_int_size configurable

--- pi-pico/jsonconf.cpp
+++ pi-pico/jsonconf.cpp
@@ -39,7 +39,7 @@ static uint8_t internal_modulation;
 static float internal_frequency;
 static float internal_shift;
 static uint16_t internal_mac_ls_bytes;
-static int xconfig_count=2;
+static int xconfig_count=3;
 
 struct config {
 	const char *name;
@@ -49,6 +49,7 @@ struct config {
 } config[]={
 	CONF_STRING32(wifi_id,CONF_wifi_id,""),
 	CONF_STRING32(wifi_passphrase,CONF_wifi_passphrase,""),
+	CONF_UINT32(client_int_size,CONF_radio_IP_size_internal,"0"),
 	CONF_BOOL(is_master,is_TDMA_master,"false"),
 	CONF_STRING13(callsign,CONF_radio_my_callsign+2,"N0CALL-1       "),
 	CONF_BOOL(telnet_last_active,is_telnet_active,"true"),
