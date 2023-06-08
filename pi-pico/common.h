9fad2c876fc7235bc72eb1819616b84acef1efb5
Martin Schaller <dh1df@darc.de>
2023-06-08 09:19:43 +0200

    Fixed usb and answer dhcp only on bridge interface

--- pi-pico/common.h
+++ pi-pico/common.h
@@ -6,6 +6,7 @@ extern void littlefs_init(void);
 extern void enchw_poll(void);
 extern void enchw_init(void);
 extern struct netif netif_eth;
+extern struct netif netif_usb;
 #ifdef __cplusplus
 }
 #endif
