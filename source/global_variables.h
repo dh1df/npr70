b8192b066d16fe303ec4084c896a639b818432d1
Martin Schaller <dh1df@darc.de>
2023-06-20 17:21:23 +0200

    Added more informative version

--- source/global_variables.h
+++ source/global_variables.h
@@ -44,7 +44,9 @@ extern ext_SRAM_chip* SPI_SRAM_p;
 	#define SI4463_NOUTDIV 8
 #endif
 
-#define FW_VERSION "2020_05_16-DH1DF"
+#ifndef FW_VERSION
+#define FW_VERSION "2020_05_16"
+#endif
 
 extern SI4463_Chip* G_SI4463;
 
